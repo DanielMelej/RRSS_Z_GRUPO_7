a6d8952015367591c0586722d0f54f1d2075a1ca
DanielMelej <da.melej@duocuc.cl>
2025-09-26 20:10:22 -0300

    AvanceHomeScreen

--- app/src/main/java/com/example/myapplication/ui/theme/HomeScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/HomeScreen.kt
@@ -1,9 +1,20 @@
 package com.example.myapplication.ui.theme
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -16,8 +27,31 @@ fun HomeScreen() {
         Column(
             modifier = Modifier
                 .padding(innerPadding)
-                .fillMazSize()
-                .padding(16.dp)
-        )
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "¡Bienvenido!")
+
+            Button(onClick = { /* acción futura */ }) {
+                Text("Presióname")
+            }
+
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo App",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp),
+                contentScale = ContentScale.Fit
+            )
+        }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen()
 }
